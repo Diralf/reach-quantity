3af60361d7e9737196daa12f5e2783bb9ac7de2b
Dzmitry_Kazlou <dzmitry_kazlou1@epam.com>
2023-01-04 14:46:51 +0300

    added test for example route

--- cypress/e2e/home.cy.ts
+++ cypress/e2e/home.cy.ts
@@ -1,5 +1,13 @@
-describe('template spec', () => {
-  it('passes', () => {
+describe('Home', () => {
+  it('Should passes', () => {
     cy.visit('http://localhost:3000');
+  });
+
+  it('Should navigate to examples', () => {
+    cy.visit('http://localhost:3000');
+    cy.get('a[href*="example"]').click();
+
+    cy.url().should('include', '/example');
+    cy.get('h1').contains('Example');
   });
 });
