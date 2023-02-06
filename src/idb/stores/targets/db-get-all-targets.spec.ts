84d566f9a733c0eba4229a68065e3114d6528ff6
Dzmitry_Kazlou <dzmitry_kazlou1@epam.com>
2023-02-06 14:12:33 +0300

    defined test for get all targets

--- src/idb/stores/targets/db-get-all-targets.spec.ts
+++ src/idb/stores/targets/db-get-all-targets.spec.ts
@@ -4,15 +4,16 @@ import { DbVersions, DB_NAME } from '../../db/db.constants';
 import { openReachQuantityDb } from '../../db/open-reach-quantity-db';
 import { DbSchema, DbStoreNames } from '../../types/db.schema';
 import { dbCreateTarget } from './db-create-target';
+import { dbGetAllTargets } from './db-get-all-targets';
 
-const { restoreTestDB, getAll } = initDbUtils<DbSchema, DbStoreNames, DbVersions>(DB_NAME, openReachQuantityDb);
+const { restoreTestDB } = initDbUtils<DbSchema, DbStoreNames, DbVersions>(DB_NAME, openReachQuantityDb);
 
 describe('dbGetAllTargets', () => {
   afterEach(async () => {
     await restoreTestDB();
   });
 
-  it('Should create target', async () => {
+  it('Should get all targets', async () => {
     const target = {
       name: 'test name',
       quantity: 5,
@@ -23,7 +24,8 @@ describe('dbGetAllTargets', () => {
 
     await dbCreateTarget(target);
     await dbCreateTarget(target);
-    const allTargets = await getAll('TARGETS');
+    await dbCreateTarget(target);
+    const allTargets = await dbGetAllTargets();
 
     expect(allTargets).toEqual([
       {
@@ -33,6 +35,10 @@ describe('dbGetAllTargets', () => {
       {
         ...target,
         id: 2,
+      },
+      {
+        ...target,
+        id: 3,
       },
     ]);
   });
