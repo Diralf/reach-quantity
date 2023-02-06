59aee5771970e257854b2404a0c096d12f953dcc
Dzmitry_Kazlou <dzmitry_kazlou1@epam.com>
2023-02-06 14:31:41 +0300

    added case for update reached to tests

--- src/idb/stores/reached/db-update-reached.spec.ts
+++ src/idb/stores/reached/db-update-reached.spec.ts
@@ -27,4 +27,25 @@ describe('dbUpdateReached', () => {
     expect(allTargets).toEqual([{ ...target, id: 1 }]);
     expect(allReached).toEqual([{ ...reachedBody, id: 1 }]);
   });
+
+  it('Should update existing reached for target', async () => {
+    const target = mockTarget;
+    const reached1: UpdateReachedEntity = { ...mockReached, quantity: 1 };
+    const reached2: UpdateReachedEntity = { ...mockReached, quantity: 3 };
+
+    await dbCreateTarget(target);
+    await dbUpdateReached(reached1);
+    const allTargets = await getAll('TARGETS');
+    const allReached = await getAll('REACHED');
+
+    expect(allTargets).toEqual([{ ...target, id: 1 }]);
+    expect(allReached).toEqual([{ ...reached1, id: 1 }]);
+
+    await dbUpdateReached(reached2);
+    const allTargets2 = await getAll('TARGETS');
+    const allReached2 = await getAll('REACHED');
+
+    expect(allTargets2).toEqual([{ ...target, id: 1 }]);
+    expect(allReached2).toEqual([{ ...reached2, id: 1 }]);
+  });
 });
