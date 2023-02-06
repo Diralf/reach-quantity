9ceff6280d741da5fb7307ceec6edd61b2b2d61b
Dzmitry_Kazlou <dzmitry_kazlou1@epam.com>
2023-02-06 10:50:54 +0300

    added tests for create

--- src/idb/idb.spec.ts
+++ src/idb/idb.spec.ts
@@ -1,49 +1,95 @@
 import 'fake-indexeddb/auto';
 import { IDBPDatabase, deleteDB } from 'idb';
-import { openReachQuantityDb, DbVersions, DB_NAME, Schema } from './idb-api-controller';
+import { SymbolicPeriod } from '../constants/symbolic-period';
+import { openReachQuantityDb, DbVersions, DB_NAME, Schema, DbObjectStores, dbCreateTarget } from './idb-api-controller';
 
-let db: IDBPDatabase<Schema>;
+const initDbUtils = (openDBCallback: (version?: DbVersions) => Promise<IDBPDatabase<Schema>>) => {
+  let testDB: IDBPDatabase<Schema>;
+
+  return {
+    async openTestDB(version?: DbVersions) {
+      testDB = await openDBCallback(version);
+      return testDB;
+    },
+    async restoreTestDB() {
+      if (testDB) {
+        testDB.close();
+      }
+      await deleteDB(DB_NAME);
+    },
+  };
+};
+
+const { openTestDB, restoreTestDB } = initDbUtils((version?: DbVersions) => openReachQuantityDb(version));
+
+const getAll = async (store: DbObjectStores) => {
+  const db = await openTestDB();
+  return db
+    .transaction([store])
+    .objectStore(store)
+    .getAll();
+};
 
 describe('idb', () => {
   afterEach(async () => {
-    if (db) {
-      db.close();
-    }
-    await deleteDB(DB_NAME);
+    await restoreTestDB();
   });
 
   describe('open IDB', () => {
     it('should update from 0 to 1 version', async () => {
-      db = await openReachQuantityDb(DbVersions.v1);
+      const db = await openTestDB(DbVersions.v1);
 
-      expect(db.objectStoreNames).toEqual(['targets']);
+      expect(db.objectStoreNames).toEqual([DbObjectStores.TARGETS]);
     });
 
     it('should update from 0 to 2 version', async () => {
-      db = await openReachQuantityDb(DbVersions.v2);
+      const db = await openTestDB(DbVersions.v2);
 
-      expect(db.objectStoreNames).toEqual(['reached', 'targets']);
+      expect(db.objectStoreNames).toEqual([DbObjectStores.REACHED, DbObjectStores.TARGETS]);
     });
 
     it('should update from 1 to 2 version', async () => {
       const db1 = await openReachQuantityDb(DbVersions.v1);
       db1.close();
-      expect(db1.objectStoreNames).toEqual(['targets']);
+      expect(db1.objectStoreNames).toEqual([DbObjectStores.TARGETS]);
 
-      db = await openReachQuantityDb(DbVersions.v2);
+      const db = await openTestDB(DbVersions.v2);
 
-      expect(db.objectStoreNames).toEqual(['reached', 'targets']);
+      expect(db.objectStoreNames).toEqual([DbObjectStores.REACHED, DbObjectStores.TARGETS]);
     });
 
     it('should handle update from 1 unclosed to 2 version', async () => {
       await openReachQuantityDb(DbVersions.v1);
-      db = await openReachQuantityDb(DbVersions.v2);
+      const db = await openTestDB(DbVersions.v2);
 
-      expect(db.objectStoreNames).toEqual(['reached', 'targets']);
+      expect(db.objectStoreNames).toEqual([DbObjectStores.REACHED, DbObjectStores.TARGETS]);
     });
   });
 
-  describe('stores', () => {
+  describe('dbCreateTarget', () => {
+    it('Should create target', async () => {
+      const target = {
+        name: 'test name',
+        quantity: 5,
+        measurement: 'tests',
+        period: SymbolicPeriod.CurrentQuarter,
+        createdOn: new Date('2023-01-01'),
+      };
 
+      await dbCreateTarget(target);
+      await dbCreateTarget(target);
+      const allTargets = await getAll(DbObjectStores.TARGETS);
+
+      expect(allTargets).toEqual([
+        {
+          ...target,
+          id: 1,
+        },
+        {
+          ...target,
+          id: 2,
+        },
+      ]);
+    });
   });
 });
